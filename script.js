c09b19c136589d8b2fa4cae8e64f928d7cdc0c3f
TechieAbHi07 <newtonabhi04@gmail.com>
2023-12-20 19:28:24 +0530

    finished project

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+    var menu =document.querySelector(".menu-links");
+    var icon =document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
